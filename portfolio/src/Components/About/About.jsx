a65b8c02cd0fb702264efa6ca8a1bbf0c8952349
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-24 14:36:37 -0300

    ok

--- portfolio/src/Components/About/About.jsx
+++ portfolio/src/Components/About/About.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './About.css'
 //import theme_pattern from '../../assets/theme_pattern.png'
-import profile_img from '../../assets/profile_img.png'
+import big_profile_img from '../../assets/big_profile_img.png'
 
 const About = () => {
   return (
     <div className='about'>
         <div className="about-title">
             <h1>About Me</h1>
-            <img src="{theme_pattern}" alt="" />
         </div>
         <div className="about-sections">
             <div className="about-left">
-                <img src={profile_img} alt="" />
+                <img src={big_profile_img} alt="" />
             </div>
             <div className="about-right">
                 <div className="about-para">
@@ -20,13 +19,31 @@ const About = () => {
                     <p>Tengo experiencia trabajando en entornos exigentes y colaborativos, con una mentalidad orientada a soluciones y a la mejora continua. Busco aportar eficiencia en el desarrollo de aplicaciones web, con un enfoque en la experiencia del usuario y las mejores prácticas del sector.</p>
                 </div>
                 <div className="about-skills">
-                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
-                    <div className="about-skill"><p>Python</p><hr style={{width:"50%"}}/></div>
-                    <div className="about-skill"><p>Django</p><hr style={{width:"50%"}}/></div>
-                    <div className="about-skill"><p>JavaScript</p><hr style={{width:"50%"}}/></div>
-                    <div className="about-skill"><p>React</p><hr style={{width:"50%"}}/></div>
+                    <div className="about-skill"><p>HTML & CSS</p></div>
+                    <div className="about-skill"><p>Python</p></div>
+                    <div className="about-skill"><p>Django</p></div>
+                    <div className="about-skill"><p>JavaScript</p></div>
+                    <div className="about-skill"><p>React</p></div>
                 </div>
             </div>
+        </div>
+        <div className="about-achievements">
+            <div className="achievement">
+                <h1>10+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="achievement">
+                <h1>90+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="achievement">
+                <h1>15+</h1>
+                <p>HAPPY CLIENTS</p>
+            </div>
+            <hr />
+
         </div>
     </div>
   )
