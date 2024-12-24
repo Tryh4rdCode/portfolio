18cb9516d6c3afca09e2c63e99127f129dbf1bb0
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-23 23:52:05 -0300

    ok

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./Components/Navbar/navbar"
+import Navbar from "./Components/Navbar/navbar";
 
 function App() {
   return (
-  <div className="bg-[#171d32] h-auto w-full overflow-hidden"> 
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden"> 
       <Navbar />
     </div>
   );
 }
 
-export default App
+export default App;
--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
 
-const Navbar = () => {
+const navbar = () => {
   return (
-    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20 '>
-        <span className='text-xl font-bold tracking-wide'>Portfolio</span>
-        <ul className='mx-24 p-y2 mt-4 font-semibold md:mt5 bg-black px-2 rounded-xl bg-=opacity-30 md:border-none 
-        text-center md:bg-transparent md:static md:mx-0 md:flex gap-6 '>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+    <div className='navbar'>
+        <img src={logo} alt=""/>
+        <ul className='nav-menu'>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Projects</a></li>
+            <li><a href='#'>Contact</a></li>
+        </ul>     
+        <div className='nav-connect'>Connect With Me</div>      
+    </div>
   )
 }
 
-export default Navbar
+export default navbar
