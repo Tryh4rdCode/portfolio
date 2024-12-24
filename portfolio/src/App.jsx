ce220ed82c1378c243bfa8d2bf8344101fa63966
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-24 01:36:33 -0300

    ok

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/navbar";
 
 function App() {
   return (
     <div className="bg-[#171d32] h-auto w-full overflow-hidden"> 
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+    <img src={profile_img} alt=""/>
+    <h1>I'm Jonatthan Medalla, Backend Developer based in Chile</h1>
+    <p>I am a Backend Developer from Santiago de Chile, recently graduated from the Inacap Professional Institute</p>
+    <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
