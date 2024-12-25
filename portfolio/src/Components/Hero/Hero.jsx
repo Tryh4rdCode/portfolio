cf393ec9723c623f9b3084765a2ccd9b31c30a67
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-24 21:11:25 -0300

    ok

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.png'
+// filepath: /c:/Users/forge/Documents/GitHub/portfolio/portfolio/src/Components/Hero/Hero.jsx
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.png';
+import CV from '../../assets/CV.pdf';
 
 const Hero = () => {
   return (
     <div className='hero'>
-    <img src={profile_img} alt=""/>
-    <h1><span>I'm Jonatthan Medalla,</span> Backend Developer based in Chile</h1>
-    <p>I am a Backend Developer from Santiago de Chile, recently graduated from the Inacap Professional Institute</p>
-    <div className="hero-action">
+      <img src={profile_img} alt="Profile" />
+      <h1><span>I'm Jonatthan Medalla,</span> Backend Developer based in Chile</h1>
+      <p>I am a Backend Developer from Santiago de Chile, recently graduated from the Inacap Professional Institute</p>
+      <div className="hero-action">
         <div className="hero-connect">Connect with me</div>
-        <div className="hero-resume" >My resume</div>
+        <a href={CV} className="hero-resume" download>My resume</a>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import About from '../About/About'
 
 const navbar = () => {
   return (
@@ -8,7 +9,7 @@ const navbar = () => {
         <img src={logo} alt=""/>
         <ul className='nav-menu'>
             <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
+            <li><a href="#about">About</a></li>
             <li><a href='#'>Projects</a></li>
             <li><a href='#'>Contact</a></li>
         </ul>     
